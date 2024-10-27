d8e8b8526b305326637ec333462b201da1b7daf0
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-10-27 11:50:31 -0500

    Created boilerplate root header.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,32 @@
+import { NavLink, Outlet } from "react-router-dom";
+
 function Root() {
   return (
-    <>
-      <p>Pablooooooo!</p>
-    </>
+    <div>
+      <div id="header">
+        <h1>
+          <NavLink to="/">Pablo Munoz-Evers</NavLink>
+        </h1>
+        <ul id="menu">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink>About</NavLink>
+          </li>
+          <li>
+            <NavLink>Writing</NavLink>
+          </li>
+          <li>
+            <NavLink>Acting</NavLink>
+          </li>
+          <li>
+            <NavLink>Coding</NavLink>
+          </li>
+        </ul>
+      </div>
+      <Outlet />
+    </div>
   );
 }
 
