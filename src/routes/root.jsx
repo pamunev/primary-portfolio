db06981b9682c9c0d52c90bd148ee337ef362597
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-11-13 23:05:58 -0600

    Styled header.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,28 +2,30 @@ import { NavLink, Outlet } from "react-router-dom";
 
 function Root() {
   return (
-    <div>
+    <div id="root-component">
       <div id="header">
         <h1>
           <NavLink to="/">Pablo Munoz-Evers</NavLink>
         </h1>
-        <ul id="menu">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/writing">Writing</NavLink>
-          </li>
-          <li>
-            <NavLink>Acting</NavLink>
-          </li>
-          <li>
-            <NavLink to="/coding">Coding</NavLink>
-          </li>
-        </ul>
+        <nav>
+          <ul id="menu">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/writing">Writing</NavLink>
+            </li>
+            <li>
+              <NavLink>Acting</NavLink>
+            </li>
+            <li>
+              <NavLink to="/coding">Coding</NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
       <Outlet />
     </div>
