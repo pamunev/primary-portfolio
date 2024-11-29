eaa9ab1fe65377cdc929e7c0993ea9373321b8a8
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-11-28 23:30:42 -0600

    Planned some of the Acting page's layout.

--- src/routes/acting.jsx
+++ src/routes/acting.jsx
@@ -1,5 +1,21 @@
 function Acting() {
-  return <div>Here will go my acting stuff.</div>;
+  return (
+    <div>
+      <p>Here will go my acting stuff.</p>
+      <p>
+        Basically, I include a summary of myself as an actor. Then link my
+        resume. Then maybe a complete list of acting roles? TBD. Then some
+        pictures, then possibly some videos, with descriptions.
+      </p>
+      <p>
+        Or maybe there are links to photos of my performances first, so I have
+        individual pages which act as photo albums for each play or production
+        that I have photos for. Or I have some selected ones in the main acting
+        page. Maybe with links to the ones that I have more photos for, like
+        baron's men plays, etc.
+      </p>
+    </div>
+  );
 }
 
 export default Acting;
