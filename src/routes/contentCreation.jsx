76fe961dc111135d7179e63408a0fa8a98fae0be
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-12-01 16:16:28 -0600

    Added captioned posts to contentCreation page.

--- src/routes/contentCreation.jsx
+++ src/routes/contentCreation.jsx
@@ -1,59 +1,37 @@
+import carDealership from "../assets/car-dealership.png";
+import cocktail from "../assets/cocktail.png";
+import landscaping from "../assets/landscaping.png";
+import hardwood from "../assets/hardwood.jpg";
+
 function ContentCreation() {
   return (
     <div>
-      <p>
-        Here will go my social media GoDaddy posts. Maybe something else too.
-        <p>
-          Here are some captions I found in my computer docs from my time in
-          GoDaddy. There is also a folder full of good stuff called GoDaddy
-          Stuff From Work Computer. I can look through that later. Some captions
-          I found today:
-        </p>{" "}
-        <p> Anything worth doing is worth doing right.(bar, making a drink)</p>
-        <p>
-          We love to kick back and read your feedback after a long day.
-          https://www.yelp.com/biz/151-kitchen-bar-elmhurst{" "}
+      <h1 className="header-content-creation">Social Media Posts</h1>
+      <div className="captioned-post">
+        <p className="caption-header">Car Dealership</p>
+        <img alt="Car Dealership" src={carDealership} />
+        <p className="caption">
+          Still has that new website smell: [URL redacted]
         </p>
-        <p>
-          Making the best sale possible requires an experienced negotiator with
-          thorough knowledge of the market. That’s where we come in.{" "}
+      </div>
+      <div className="captioned-post">
+        <p className="caption-header">Cocktail Bar</p>
+        <img alt="Cocktail" src={cocktail} />
+        <p className="caption">Anything worth doing is worth doing right.</p>
+      </div>
+      <div className="captioned-post">
+        <p className="caption-header">Landscaping Company</p>
+        <img alt="Landscaping" src={landscaping} />
+        <p className="caption">Designing your personal paradise.</p>
+      </div>
+      <div className="captioned-post">
+        <p className="caption-header">Flooring Company</p>
+        <img alt="Hardwood Floors" src={hardwood} />
+        <p className="caption">
+          Even the most beautiful hardwood floors will show the marks of time.
+          Let us rejuvenate yours.
         </p>
-        <p>
-          Still has that new website smell: [redacted auto sales company
-          website]{" "}
-        </p>
-        <p>
-          Whether you buy or sell, there's a story to tell. (real estate agent?){" "}
-        </p>
-        <p>
-          Rediscover nature. Rediscover yourself. (some camping thing or
-          something?){" "}
-        </p>
-        <p>
-          Every problem has a solution. It's up to us to find it. (Mechanic?){" "}
-        </p>
-        <p>
-          All positive change in life is made up of little adjustments.
-          (Something like this. Maybe for a gym. Maybe rephrase a little.){" "}
-        </p>
-        <p>
-          Learning life's lessons is better with friends. (image of kids
-          smiling. Maybe for a preschool or daycare?){" "}
-        </p>
-        <p>
-          All who live great lives need some mentors along the way. (Unsure what
-          this was for, but he gave it a great.)
-        </p>
-      </p>
-      <p>
-        I can choose my favorites from here and from that other folder, and
-        others I find around. Then make some stock-photo images to go with them
-        on canva. Find out the dimensions I want.
-      </p>
-      <p>
-        The other folder is the GoDaddy desktop things. It's called something
-        like that.
-      </p>
+      </div>
     </div>
   );
 }
